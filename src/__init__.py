e7fef0c54441c4225929cfb1e5d9c250b5e51725
dafrose-bajaro <dafrose.bajaro@gmail.com>
2025-04-20 21:42:08 +0800

    chore: update src files

---
+++ src/__init__.py
@@ -0,0 +1,38 @@
+# libraries to make functions work
+import pandas as pd
+import geopandas as gpd
+import rasterio
+from rasterio import features
+from rasterio.plot import show
+from loguru import logger
+
+# configure loguru logger
+logger.add("logs.log", format="{time} {level} {message}", level="INFO", rotation="1 MB", compression="zip")
+
+# open csv as dataframe
+def open_csv(file_path):
+    try:
+        logger.info(f"Opening CSV file: {file_path}")
+        return pd.read_csv(file_path)
+    except Exception as e:
+        logger.error(f"Failed to open CSV file: {file_path} - Error: {e}")
+        raise e
+
+# open vector data (geojson, shp) as dataframe
+def open_vector(file_path):
+    try:
+        logger.info(f"Opening vector file: {file_path}")
+        return gpd.read_file(file_path)
+    except Exception as e:
+        logger.error(f"Failed to open vector file: {file_path} - Error: {e}")
+        raise e
+
+# display raster as image
+def display_raster(file_path):
+    try:
+        logger.info(f"Opening and displaying raster file: {file_path}")
+        with rasterio.open(file_path) as src:
+            show(src)  # Display the raster visually
+    except Exception as e:
+        logger.error(f"Failed to open or display raster file: {file_path} - Error: {e}")
+        raise e
--- src/imports_geospatial.py
+++ src/imports_geospatial.py
@@ -1,4 +1,8 @@
-# geospatial raster operations
+# general geospatial
+import geopandas as gpd #used for converting and operating on geodataframes
+import geowrangler #used for geospatial operations
+
+# raster operations
 import rasterio as rio #reads and writes geotiffs based on numpy arrays and geojson
 from rasterio.plot import show #simple plot generation
 from rasterio.plot import show_hist #simple histogram generation
@@ -10,11 +14,8 @@ from earthpy import spatial as es #needed for stacking rasters
 from earthpy import mask as em #masking raster pixels such as in cloudmasking
 from earthpy import plot as ep #plotting several bands of a raster
 from rasterstats import zonal_stats #raster zonal stats
-from geowrangler import raster_zonal_stats as rzs #raster zonal stats using geowrangler
 
-# geospatial vector operations
-import geopandas as gpd #used for converting and operating on geodataframes
+# vector operations
 import fiona #used for reading and writing vector data
 import shapely #used for creating and operating on shapely geometries
 from shapely.errors import ShapelyDeprecationWarning #prevent shapely deprecation warnings from showing
-import geopandas as gpd #used for converting and operating on geodataframes
