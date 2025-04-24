88f7510c965f3efd53f2968ed82b2a56bcaf4c26
dafrose-bajaro <dafrose.bajaro@gmail.com>
2025-04-24 22:05:23 +0800

    chore: add assets for nasa_firms

---
+++ src/assets/nasa_firms/assets.py
@@ -0,0 +1,50 @@
+import io
+
+import dagster as dg
+import polars as pl
+from dagster import MetadataValue
+from httpx import AsyncClient
+
+from src.partitions import daily_partitions_def
+from src.resources import IOManager
+from src.settings import settings
+
+
+# asset # 1: nasa_firms_raw
+# fetches raw data from NASA FIRMS API
+@dg.asset(
+    partitions_def=daily_partitions_def,
+    io_manager_key=IOManager.GCS.value,
+    kinds={"gcs"},
+)
+async def nasa_firms_raw(context: dg.AssetExecutionContext) -> str:
+    async with AsyncClient(base_url=settings.NASA_FIRMS_BASE_URL) as client:
+        res = await client.get(
+            f"/api/country/csv/{settings.NASA_FIRMS_MAP_KEY}/VIIRS_SNPP_NRT/THA/1/{context.partition_key}"
+        )
+        res.raise_for_status()
+        text = res.text
+
+    context.add_output_metadata({"size": MetadataValue.int(len(text))})
+    return text
+
+
+# asset #2: nasa_firms_raw_delta
+# transforms raw data into a polars dataframe
+@dg.asset(
+    partitions_def=daily_partitions_def,
+    io_manager_key=IOManager.DELTALAKE.value,
+    metadata={
+        "partition_expr": "measurement_date",
+    },
+    kinds={"gcs", "polars", "deltalake"},
+)
+def nasa_firms_raw_delta(
+    context: dg.AssetExecutionContext,
+    nasa_firms_raw: str,
+) -> pl.DataFrame:
+    with io.StringIO(nasa_firms_raw) as buf:
+        df = pl.read_csv(buf)
+
+    df = df.with_columns(measurement_date=pl.lit(context.partition_key).cast(pl.Date()))
+    return df
